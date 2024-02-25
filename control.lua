550e607c6c4db9b88d2e4dbe8ff3bf9def136540
HEROgold <martijnwieringa28@gmail.com>
2024-02-25 22:52:25 +0100

    added a way to de-level ghosts.

--- control.lua
+++ control.lua
@@ -6,7 +6,8 @@ local max_speed_level = tonumber(settings.startup["battery-roboport-energy-resea
 local update_timer = tonumber(settings.startup["battery-roboport-update-timer"].value)
 local upgrade_timer = tonumber(settings.startup["battery-roboport-upgrade-timer"].value)
 local roboports_to_upgrade = {}
-
+local roboports_to_downgrade = {}
+local mod_roboport_name = "battery-roboport-mk-"
 
 script.on_init(
     function ()
@@ -40,7 +41,7 @@ function Is_valid_roboport(roboport)
     if r_name == "roboport" then
         -- The entity is from vanilla Factorio
         return true
-    elseif utils.starts_with(r_name, "battery-roboport-mk-") then
+    elseif utils.starts_with(r_name, mod_roboport_name) then
         local level = Get_level_from_name(r_name)
         -- Check for correct levels, to avoid replacing already correct roboports.
         if level[1] < global.EffectivityResearchLevel or level[2] < global.ProductivityResearchLevel or level[3] < global.SpeedResearchLevel then
@@ -60,40 +61,66 @@ function Is_research_valid()
     return eff and prod and speed
 end
 
--- End of helper functions
--- Start of main functions
 
 local function upgrade_roboport()
-    -- ---@param roboport LuaEntity
     local roboport, needs_upgrade = next(roboports_to_upgrade)
     if roboport == nil then
         return
     end
-        if roboport.valid and needs_upgrade == true then
-            local surface = roboport.surface
-            if Is_valid_roboport(roboport) then
-                local old_energy = roboport.energy
-                local suffix = utils.get_internal_suffix(global.EffectivityResearchLevel, global.ProductivityResearchLevel, global.SpeedResearchLevel)
+    if roboport.valid and needs_upgrade == true then
+        local surface = roboport.surface
+        if Is_valid_roboport(roboport) then
+            local old_energy = roboport.energy
+            local suffix = utils.get_internal_suffix(global.EffectivityResearchLevel, global.ProductivityResearchLevel, global.SpeedResearchLevel)
 
-                local to_create = {
-                    name = "battery-roboport-mk-" .. suffix,
-                    position = roboport.position,
-                    force = roboport.force,
-                    fast_replace = true,
-                    spill = false,
-                    create_build_effect_smoke = false,
-                }
-                local created_rport = surface.create_entity(to_create)
-                created_rport.energy = old_energy
-                roboports_to_upgrade[roboport] = nil
-
-                roboport.destroy()
-                end
-        else
+            local to_create = {
+                name = mod_roboport_name .. suffix,
+                position = roboport.position,
+                force = roboport.force,
+                fast_replace = true,
+                spill = false,
+                create_build_effect_smoke = false,
+            }
+            local created_rport = surface.create_entity(to_create)
+            created_rport.energy = old_energy
             roboports_to_upgrade[roboport] = nil
-        end
+
+            roboport.destroy()
+            end
+    else
+        roboports_to_upgrade[roboport] = nil
+    end
 end
 
+local function downgrade_ghost_roboport()
+    local roboport, needs_downgrade = next(roboports_to_downgrade)
+    if roboport == nil then
+        return
+    end
+    if roboport.valid and needs_downgrade == true then
+        local surface = roboport.surface
+        if roboport.name == "entity-ghost" and utils.starts_with(roboport.ghost_name, mod_roboport_name) then
+            local to_create = {
+                name = "entity-ghost",
+                type = "entity-ghost",
+                ghost_name = "roboport",
+                ghost_type = "roboport",
+                ghost_prototype = "roboport",
+                position = roboport.position,
+                force = roboport.force,
+                fast_replace = true,
+                spill = false,
+                create_build_effect_smoke = false,
+            }
+            local created_rport = surface.create_entity(to_create)
+            roboports_to_downgrade[roboport] = nil
+
+            roboport.destroy()
+            end
+    else
+        roboports_to_downgrade[roboport] = nil
+    end
+end
 
 local function mark_roboports_for_upgrade()
     for _, surface in pairs(game.surfaces) do
@@ -140,18 +167,28 @@ local function mark_roboport_for_upgrade(roboport)
     end
 end
 
-script.on_event(defines.events.on_built_entity,
-function (event)
-    mark_roboport_for_upgrade(event.created_entity)
-end)
 
-script.on_event(defines.events.on_robot_built_entity,
-function (event)
-    mark_roboport_for_upgrade(event.created_entity)
-end)
+local function mark_roboport_for_downgrade(roboport)
+    roboports_to_downgrade[roboport] = true
+end
+
+
+local function on_built(event)
+    if event.created_entity.name == "entity-ghost" or event.created_entity.type == "entity-ghost" then
+        mark_roboport_for_downgrade(event.created_entity)
+    else
+        mark_roboport_for_upgrade(event.created_entity)
+    end
+end
+
+script.on_event(defines.events.on_built_entity, on_built)
+script.on_event(defines.events.on_robot_built_entity, on_built)
 
 
 script.on_nth_tick(
     upgrade_timer,
-    upgrade_roboport
+    function ()
+        upgrade_roboport()
+        downgrade_ghost_roboport()
+    end
 )
