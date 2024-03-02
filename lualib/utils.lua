4e5f3415a40a31aee51c56987c9c76480f90317c
HEROgold <martijnwieringa28@gmail.com>
2024-03-02 03:31:00 +0100

    updated utils

--- lualib/utils.lua
+++ lualib/utils.lua
@@ -1,6 +1,6 @@
 utils = {}
 
-function utils.add_technology_icon_constant_battery(technology_icon)
+function utils.sprite_add_icon(technology_icon, module_icon)
   local icons =
   {
     {
@@ -8,23 +8,16 @@ function utils.add_technology_icon_constant_battery(technology_icon)
       icon_size = 256, icon_mipmaps = 4
     },
     {
-      icon = "__core__/graphics/icons/technology/constants/constant-battery.png",
-      icon_size = 128,
+      icon = module_icon,
+      icon_size = 64,
       icon_mipmaps = 3,
-      shift = {100, 100}
+      shift = {80, 100},
+      scale = 1.5
     }
   }
   return icons
 end
 
-
-function utils.table_length(table)
-  local len = 0
-  for _ in pairs(table) do
-    len = len + 1
-  end
-  return len
-end
 
 function utils.starts_with(to_check, target)
   return string.sub(to_check, 1, string.len(target)) == target
