46b5a66f4efed63affc1614b435df39848363ca6
HEROgold <martijnwieringa28@gmail.com>
2024-03-02 03:30:33 +0100

    added psuedocode

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,9 @@
 -- Add the replaceable_group to vanilla roboport
 if data.raw["roboport"]["roboport"] then
     data.raw["roboport"]["roboport"].fast_replaceable_group = "roboport"
+    -- if "upgrade modded" then
+    --     for a, b in pairs(data.raw["roboport"]) do
+    --         data.raw["roboport"][a].fast_replaceable_group = a
+    --     end
+    -- end
 end
