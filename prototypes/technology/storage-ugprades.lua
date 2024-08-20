6da23f54466e0910b19ee07e4b57fee0e7fdd2f1
HEROgold <martijnwieringa28@gmail.com>
2024-08-20 02:57:06 +0200

    code setup

--- prototypes/technology/storage-ugprades.lua
+++ prototypes/technology/storage-ugprades.lua
@@ -10,6 +10,28 @@ local modules = data.raw["module"]
 
 local f = {}
 
+f.get_research_prerequisites = function(upgrade_name, level)
+  local prerequisites = nil
+
+  if level == 1 then
+    prerequisites = {
+      "logistic-robotics"
+    }
+  else
+    prerequisites = {
+      f.get_research_name(upgrade_name, level-1)
+    }
+  end
+  return prerequisites
+end
+
+f.get_tech_sprite = function (module_type, level)
+    return utilities.technology_sprite_add_item_icon(
+      "__base__/graphics/technology/robotics.png",
+      "__space-exploration-graphics__/graphics/icons/modules/"..module_type.."-"..level..".png"
+    )
+end
+
 f.get_research_name = function(upgrade_name, level)
   return "roboport-" .. upgrade_name .. f.get_suffix_by_level(level)
 end
