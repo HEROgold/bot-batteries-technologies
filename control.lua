73593bc51b906572d9b15c0bde225d038d0e97a3
HEROgold <martijnwieringa28@gmail.com>
2024-10-23 14:32:18 +0200

    2.0 update

--- control.lua
+++ control.lua
@@ -16,34 +16,34 @@ script.on_init(
 
 
 function Setup_Vars()
-    if global.EffectivityResearchLevel == nil then
-        global.EffectivityResearchLevel = 0
+    if storage.EfficiencyResearchLevel == nil then
+        storage.EfficiencyResearchLevel = 0
     end
-    if global.ProductivityResearchLevel == nil then
-        global.ProductivityResearchLevel = 0
+    if storage.ProductivityResearchLevel == nil then
+        storage.ProductivityResearchLevel = 0
     end
-    if global.SpeedResearchLevel == nil then
-        global.SpeedResearchLevel = 0
+    if storage.SpeedResearchLevel == nil then
+        storage.SpeedResearchLevel = 0
     end
-    if global.roboports_to_update == nil then
+    if storage.roboports_to_update == nil then
         ---@type table<LuaEntity, boolean>
-        global.roboports_to_update = {}
+        storage.roboports_to_update = {}
     end
-    if global.ghosts_to_update == nil then
+    if storage.ghosts_to_update == nil then
         ---@type table<LuaEntity, boolean>
-        global.ghosts_to_update = {}
+        storage.ghosts_to_update = {}
     end
-    if global.ConstructionAreaResearchLevel == nil then
-        global.ConstructionAreaResearchLevel = 0
+    if storage.ConstructionAreaResearchLevel == nil then
+        storage.ConstructionAreaResearchLevel = 0
     end
-    if global.LogisticAreaResearchLevel == nil then
-        global.LogisticAreaResearchLevel = 0
+    if storage.LogisticAreaResearchLevel == nil then
+        storage.LogisticAreaResearchLevel = 0
     end
-    if global.RobotStorageResearchLevel == nil then
-        global.RobotStorageResearchLevel = 0
+    if storage.RobotStorageResearchLevel == nil then
+        storage.RobotStorageResearchLevel = 0
     end
-    if global.MaterialStorageResearchLevel == nil then
-        global.MaterialStorageResearchLevel = 0
+    if storage.MaterialStorageResearchLevel == nil then
+        storage.MaterialStorageResearchLevel = 0
     end
 end
 
@@ -69,7 +69,7 @@ local function validate_roboport(roboport)
         return
     end
     if not roboport.valid then
-        global.roboports_to_update[roboport] = nil
+        storage.roboports_to_update[roboport] = nil
         return
     end
 
@@ -81,19 +81,19 @@ local function validate_roboport(roboport)
         local level = energy_levels_from_name(roboport_name)
         -- Check for correct levels, to avoid replacing already correct roboports.
         if (
-            level[1] < global.EffectivityResearchLevel
-            or level[2] < global.ProductivityResearchLevel
-            or level[3] < global.SpeedResearchLevel
+            level[1] < storage.EfficiencyResearchLevel
+            or level[2] < storage.ProductivityResearchLevel
+            or level[3] < storage.SpeedResearchLevel
         ) then
             return true
         end
     elseif utilities.string_starts_with(roboport_name, storage_roboport_name_leved) then 
         local levels = storage_levels_from_name(roboport_name)
         if (
-            levels[1] < global.ConstructionAreaResearchLevel
-            or levels[2] < global.LogisticAreaResearchLevel
-            or levels[3] < global.RobotStorageResearchLevel
-            or levels[4] < global.MaterialStorageResearchLevel
+            levels[1] < storage.ConstructionAreaResearchLevel
+            or levels[2] < storage.LogisticAreaResearchLevel
+            or levels[3] < storage.RobotStorageResearchLevel
+            or levels[4] < storage.MaterialStorageResearchLevel
         ) then
             return true
         end
@@ -118,7 +118,7 @@ local function validate_ghost(ghost)
         or not utilities.string_starts_with(ghost.ghost_name, energy_roboport_name_leveled)
         and not utilities.string_starts_with(ghost.ghost_name, storage_roboport_name_leved)
     ) then
-        global.ghosts_to_update[ghost] = nil
+        storage.ghosts_to_update[ghost] = nil
         return false
     end
     return true
@@ -135,9 +135,9 @@ local function update_energy_roboport_level(roboport)
     local surface = roboport.surface
     local old_energy = roboport.energy
     local suffix = utils.get_energy_suffix(
-        global.EffectivityResearchLevel,
-        global.ProductivityResearchLevel,
-        global.SpeedResearchLevel
+        storage.EfficiencyResearchLevel,
+        storage.ProductivityResearchLevel,
+        storage.SpeedResearchLevel
     )
 
     local created_rport = surface.create_entity{
@@ -151,7 +151,7 @@ local function update_energy_roboport_level(roboport)
     }
 
     created_rport.energy = old_energy
-    global.roboports_to_update[roboport] = nil
+    storage.roboports_to_update[roboport] = nil
 
     roboport.destroy()
 end
@@ -161,10 +161,10 @@ local function update_storage_roboport_level(roboport)
     local surface = roboport.surface
     local old_energy = roboport.energy
     local storage_suffix = utils.get_storage_suffix(
-        global.ConstructionAreaResearchLevel,
-        global.LogisticAreaResearchLevel,
-        global.RobotStorageResearchLevel,
-        global.MaterialStorageResearchLevel
+        storage.ConstructionAreaResearchLevel,
+        storage.LogisticAreaResearchLevel,
+        storage.RobotStorageResearchLevel,
+        storage.MaterialStorageResearchLevel
     )
 
     local created_rport = surface.create_entity{
@@ -178,7 +178,7 @@ local function update_storage_roboport_level(roboport)
     }
 
     created_rport.energy = old_energy
-    global.roboports_to_update[roboport] = nil
+    storage.roboports_to_update[roboport] = nil
 
     roboport.destroy()
 end
@@ -194,10 +194,10 @@ local function update_ghost_level(roboport)
 
     if utilities.string_starts_with(roboport.ghost_name, storage_roboport_name_leved) then
         local suffix = utils.get_storage_suffix(
-            global.ConstructionAreaResearchLevel,
-            global.LogisticAreaResearchLevel,
-            global.RobotStorageResearchLevel,
-            global.MaterialStorageResearchLevel
+            storage.ConstructionAreaResearchLevel,
+            storage.LogisticAreaResearchLevel,
+            storage.RobotStorageResearchLevel,
+            storage.MaterialStorageResearchLevel
         )
         to_create = {
             name = "entity-ghost",
@@ -214,9 +214,9 @@ local function update_ghost_level(roboport)
         }
     elseif utilities.string_starts_with(roboport.ghost_name, energy_roboport_name_base) then
         local suffix = utils.get_energy_suffix(
-            global.EffectivityResearchLevel,
-            global.ProductivityResearchLevel,
-            global.SpeedResearchLevel
+            storage.EfficiencyResearchLevel,
+            storage.ProductivityResearchLevel,
+            storage.SpeedResearchLevel
         )
         to_create = {
             name = "entity-ghost",
@@ -235,7 +235,7 @@ local function update_ghost_level(roboport)
 
     local surface = roboport.surface
     local created_rport = surface.create_entity(to_create)
-    global.ghosts_to_update[roboport] = nil
+    storage.ghosts_to_update[roboport] = nil
 
     roboport.destroy()
 end
@@ -256,7 +256,7 @@ end
 
 
 local function tick_update_roboport_level()
-    local roboport, needs_upgrade = next(global.roboports_to_update)
+    local roboport, needs_upgrade = next(storage.roboports_to_update)
     if needs_upgrade then
         update_roboport_level(roboport)
     end
@@ -264,7 +264,7 @@ end
 
 
 local function tick_update_ghost_level()
-    local roboport, needs_downgrade = next(global.ghosts_to_update)
+    local roboport, needs_downgrade = next(storage.ghosts_to_update)
     if needs_downgrade then
         update_ghost_level(roboport)
     end
@@ -276,7 +276,7 @@ local function mark_all_roboports_for_update()
         -- TODO: This should be optimized to only check roboports that are actually affected by the research.
         -- Or seperate out energy and storage roboports, so we can only check the relevant ones.
         for _, roboport in pairs(surface.find_entities_filtered{type = "roboport"}) do
-            global.roboports_to_update[roboport] = true
+            storage.roboports_to_update[roboport] = true
         end
     end
 end
@@ -285,26 +285,26 @@ end
 script.on_event(defines.events.on_research_finished,
     ---@param event EventData.on_research_finished
     function (event)
-        if utilities.string_starts_with(event.research.name, "roboport-effectivity") then
-            global.EffectivityResearchLevel = global.EffectivityResearchLevel + 1
+        if utilities.string_starts_with(event.research.name, "roboport-efficiency") then
+            storage.EfficiencyResearchLevel = storage.EfficiencyResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-productivity") then
-            global.ProductivityResearchLevel = global.ProductivityResearchLevel + 1
+            storage.ProductivityResearchLevel = storage.ProductivityResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-speed") then
-            global.SpeedResearchLevel = global.SpeedResearchLevel + 1
+            storage.SpeedResearchLevel = storage.SpeedResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-construction-area") then
-            global.ConstructionAreaResearchLevel = global.ConstructionAreaResearchLevel + 1
+            storage.ConstructionAreaResearchLevel = storage.ConstructionAreaResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-logistics-area") then
-            global.LogisticAreaResearchLevel = global.LogisticAreaResearchLevel + 1
+            storage.LogisticAreaResearchLevel = storage.LogisticAreaResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-material-storage") then
-            global.MaterialStorageResearchLevel = global.MaterialStorageResearchLevel + 1
+            storage.MaterialStorageResearchLevel = storage.MaterialStorageResearchLevel + 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-robot-storage") then
-            global.RobotStorageResearchLevel = global.RobotStorageResearchLevel + 1
+            storage.RobotStorageResearchLevel = storage.RobotStorageResearchLevel + 1
             mark_all_roboports_for_update()
         end
     end
@@ -314,26 +314,26 @@ script.on_event(defines.events.on_research_finished,
 script.on_event(defines.events.on_research_reversed,
     ---@param event EventData.on_research_reversed
     function (event)
-        if utilities.string_starts_with(event.research.name, "roboport-effectivity") then
-            global.EffectivityResearchLevel = global.EffectivityResearchLevel - 1
+        if utilities.string_starts_with(event.research.name, "roboport-efficiency") then
+            storage.EfficiencyResearchLevel = storage.EfficiencyResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-productivity") then
-            global.ProductivityResearchLevel = global.ProductivityResearchLevel - 1
+            storage.ProductivityResearchLevel = storage.ProductivityResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-speed") then
-            global.SpeedResearchLevel = global.SpeedResearchLevel - 1
+            storage.SpeedResearchLevel = storage.SpeedResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-construction-area") then
-            global.ConstructionAreaResearchLevel = global.ConstructionAreaResearchLevel - 1
+            storage.ConstructionAreaResearchLevel = storage.ConstructionAreaResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-logistics-area") then
-            global.LogisticAreaResearchLevel = global.LogisticAreaResearchLevel - 1
+            storage.LogisticAreaResearchLevel = storage.LogisticAreaResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-material-storage") then
-            global.MaterialStorageResearchLevel = global.MaterialStorageResearchLevel - 1
+            storage.MaterialStorageResearchLevel = storage.MaterialStorageResearchLevel - 1
             mark_all_roboports_for_update()
         elseif utilities.string_starts_with(event.research.name, "roboport-robot-storage") then
-            global.RobotStorageResearchLevel = global.RobotStorageResearchLevel - 1
+            storage.RobotStorageResearchLevel = storage.RobotStorageResearchLevel - 1
             mark_all_roboports_for_update()
         end
     end
@@ -389,7 +389,7 @@ script.on_event(
         if event.entity == nil or not event.entity.valid then
             return
         end
-        global.roboports_to_update[event.entity] = nil
+        storage.roboports_to_update[event.entity] = nil
     end
 )
 
@@ -406,14 +406,14 @@ commands.add_command(
     "Shows the current research levels",
     function ()
         game.print("-- Energy --")
-        game.print("Effectivity: " .. global.EffectivityResearchLevel)
-        game.print("Productivity: " .. global.ProductivityResearchLevel)
-        game.print("Speed: " .. global.SpeedResearchLevel)
+        game.print("efficiency: " .. storage.EfficiencyResearchLevel)
+        game.print("Productivity: " .. storage.ProductivityResearchLevel)
+        game.print("Speed: " .. storage.SpeedResearchLevel)
         game.print("-- Storage --")
-        game.print("Construction Area: " .. global.ConstructionAreaResearchLevel)
-        game.print("Logistics Area: " .. global.LogisticAreaResearchLevel)
-        game.print("Robot Storage: " .. global.RobotStorageResearchLevel)
-        game.print("Material Storage: " .. global.MaterialStorageResearchLevel)
+        game.print("Construction Area: " .. storage.ConstructionAreaResearchLevel)
+        game.print("Logistics Area: " .. storage.LogisticAreaResearchLevel)
+        game.print("Robot Storage: " .. storage.RobotStorageResearchLevel)
+        game.print("Material Storage: " .. storage.MaterialStorageResearchLevel)
     end
 )
 commands.add_command(
