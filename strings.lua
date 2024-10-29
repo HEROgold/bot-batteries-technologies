b73f7dd16a577bdf8e503bd803a58e5045826b89
HEROgold <martijnwieringa28@gmail.com>
2024-10-29 14:45:52 +0100

    more utils!

--- strings.lua
+++ strings.lua
@@ -1,14 +1,58 @@
-EntityGhost = "entity-ghost"
+---Used to combine strings within '.. "-" ..' to avoid typos
+---@param names string[]
+---@return string
+local function combine(names)
+    local new = ""
+    for _, name in pairs(names) do
+        new = new .. "-" .. name
+    end
+    return new
+end
+
+Research = "research"
+Upgrade = "upgrade"
+Modifier = "modifier"
+Limit = "limit"
+Minimum = "minimum"
+Energy = "energy"
+Entity = "entity"
 Roboport = "roboport"
+Storage = "storage"
+Robot = "robot"
+Material = "material"
+Mark = "mk"
+
+
+-- Combined
+RoboportResearch = combine{Roboport, Research}
+RoboportResearchUpgrade = combine{RoboportResearch, Upgrade}
+RoboportMaterialStorage = combine{Roboport, Material, Storage}
+RoboportRobotStorage = combine{Roboport, Robot, Storage}
+RoboportEnergy = combine{Roboport, Energy}
+RoboportEnergyLeveled = combine{RoboportEnergy, Mark}
+RoboportLogisticalLeveled = combine{RoboportLogistical, Mark}
+EntityGhost = Entity .. "-ghost"
 RoboportEfficiency = Roboport .. "-efficiency"
 RoboportProductivity = Roboport .. "-productivity"
 RoboportSpeed = Roboport .. "-speed"
 RoboportConstructionArea = Roboport .. "-construction-area"
 RoboportLogisticsArea = Roboport .. "-logistics-area"
-RoboportMaterialStorage = Roboport .. "-material-storage"
-RoboportRobotStorage = Roboport .. "-robot-storage"
 RoboportUpdater = Roboport .. "-updater"
-RoboportEnergy = Roboport .. "-energy"
 RoboportLogistical = Roboport .. "-logistical"
-RoboportEnergyLeveled = RoboportEnergy .. "-mk-"
-RoboportLogisticalLeveled = RoboportLogistical .. "-mk-"
+
+-- Settings
+RobotStorageLimit = combine{Robot, Storage, Limit}
+EnergyResearchMinimum =  combine{Energy, Research, Minimum}
+EnergyResearchLimit = combine{Energy, Research, Limit}
+InputFlowLimitModifier ="input-flow-" .. {Limit, Modifier}
+BufferCapacityModifier ="buffer-capacity-" .. Modifier
+RechargeMinimumModifier ="recharge-" .. {Minimum, Modifier}
+EnergyUsageModifier = Energy .. "-usage-" .. Modifier
+ChargingEnergyModifier ="charging-energy-" .. Modifier
+ConstructionAreaLimit ="construction-area-" .. Limit
+LogisticAreaLimit ="logistic-area-" .. Limit
+MaterialStorageLimit ="material-storage-" .. Limit
+RoboportResearchUpgradeCost = RoboportResearchUpgrade .. "cost"
+RoboportResearchUpgradetime = RoboportResearchUpgrade .. "time"
+UpgradeTimer = Upgrade .. "-timer"
+ShowItems = "show-items"
