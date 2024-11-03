083782d749ad1b5df7e54cdfa9309c3897d19c9b
HEROgold <martijnwieringa28@gmail.com>
2024-11-03 13:45:05 +0100

    add some more settings

--- vars/settings.lua
+++ vars/settings.lua
@@ -13,3 +13,13 @@ material_storage_limit = settings.startup["material-storage-limit"].value
 energy_research_limit = settings.startup["energy-research-limit"].value
 ---@type number
 energy_research_minimum = settings.startup["energy-research-minimum"].value
+---@type number
+input_flow_limit_modifier = settings.startup["input-flow-limit-modifier"].value
+---@type number
+buffer_capacity_modifier = settings.startup["buffer-capacity-modifier"].value
+---@type number
+recharge_minimum_modifier = settings.startup["recharge-minimum-modifier"].value
+---@type number
+energy_usage_modifier = settings.startup["energy-usage-modifier"].value
+---@type number
+charging_energy_modifier = settings.startup["charging-energy-modifier"].value
