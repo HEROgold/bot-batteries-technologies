196032ab424eae055d18f6c9964ed55b3e0b3ec9
HEROgold <martijnwieringa28@gmail.com>
2025-12-21 13:37:32 +0100

    save progress

--- prototypes/robot/architect-robots.lua
+++ prototypes/robot/architect-robots.lua
@@ -2,36 +2,45 @@ require("__heroic-library__.utilities")
 require("helpers.suffix")
 require("vars.settings")
 
+--- Create Architect Robots
+--- @param c number Cargo level
+--- @param s number Speed level
+--- @param e number Energy level
+local function create_architect_robot(c, s, e)
+    local robot_item = table.deepcopy(architect_item)
+    local robot_entity = table.deepcopy(architect_entity)
+
+    local suffix = get_robot_suffix({cargo = c, speed = s, energy = e})
+    local name = combine{ArchitectRobotLeveled, suffix}
+
+    robot_entity.localised_name = {"entity-name." .. ArchitectRobotLeveled, tostring(c), tostring(s), tostring(e)}
+    robot_item.localised_name = {"item-name." .. ArchitectRobotLeveled, tostring(c), tostring(s), tostring(e)}
+
+    robot_item.name = name
+    robot_entity.name = name
+    robot_item.hidden = true
+
+    robot_item.place_result = robot_entity.name
+
+    robot_entity.fast_replaceable_group = ConstructionRobot
+    robot_entity.minable = robot_entity.minable
+    robot_entity.minable.result = robot_item.name
+
+    robot_entity.max_payload_size = c + (architect_entity.max_payload_size * c * modifier_max_payload_size)
+    robot_entity.speed = s + (architect_entity.speed * s * modifier_speed)
+
+    -- TODO: Move the suffix/energy numbers to the HeroicLibrary, on energy.lua
+    local energy = tonumber(string.match(robot_entity.max_energy, "%d+"))
+    local suffix = string.match(robot_entity.max_energy, "%a+")
+    robot_entity.max_energy = e + (energy * e * modifier_max_energy) .. suffix
+    return robot_item, robot_entity
+end
 
 function add_architect_robots()
     for c=0, robot_cargo_research_limit do
         for s=0, robot_speed_research_limit do
             for e=0, robot_energy_research_limit do
-                local robot_item = table.deepcopy(architect_item)
-                local robot_entity = table.deepcopy(architect_entity)
-
-                local suffix = get_robot_suffix({cargo = c, speed = s, energy = e})
-                local name = combine{ArchitectRobotLeveled, suffix}
-
-                robot_entity.localised_name = {"entity-name." .. ArchitectRobotLeveled, tostring(c), tostring(s), tostring(e)}
-                robot_item.localised_name = {"item-name." .. ArchitectRobotLeveled, tostring(c), tostring(s), tostring(e)}
-
-                robot_item.name = name
-                robot_entity.name = name
-                robot_item.hidden = true
-
-                robot_item.place_result = robot_entity.name
-
-                robot_entity.fast_replaceable_group = ConstructionRobot
-                robot_entity.minable = robot_entity.minable
-                robot_entity.minable.result = robot_item.name
-
-                robot_entity.max_payload_size = c + architect_entity.max_payload_size * c * modifier_max_payload_size
-                robot_entity.speed = s + architect_entity.speed * s * modifier_speed
-
-                local energy = tonumber(string.match(robot_entity.max_energy, "%d+"))
-                local suffix = string.match(robot_entity.max_energy, "%a+")
-                robot_entity.max_energy = e + energy * e * modifier_max_energy .. suffix
+                robot_item, robot_entity = create_architect_robot(c, s, e)
 
                 if settings.startup[ShowItems].value == true then
                     robot_item.subgroup = ItemSubGroupRobot
--- prototypes/robot/hauler-robots.lua
+++ prototypes/robot/hauler-robots.lua
@@ -2,35 +2,43 @@ require("__heroic-library__.utilities")
 require("helpers.suffix")
 require("vars.settings")
 
+--- Create Architect Robots
+--- @param c number Cargo level
+--- @param s number Speed level
+--- @param e number Energy level
+local function create_hauler_robot(c, s, e)
+    local robot_item = table.deepcopy(hauler_item)
+    local robot_entity = table.deepcopy(hauler_entity)
+
+    local suffix = get_robot_suffix({cargo = c, speed = s, energy = e})
+    local name = combine{HaulerRobotLeveled, suffix}
+
+    robot_entity.localised_name = {"entity-name." .. HaulerRobotLeveled, tostring(c), tostring(s), tostring(e)}
+    robot_item.localised_name = {"item-name." .. HaulerRobotLeveled, tostring(c), tostring(s), tostring(e)}
+
+    robot_item.name = name
+    robot_entity.name = name
+    robot_item.hidden = true
+
+    robot_item.place_result = robot_entity.name
+    robot_entity.fast_replaceable_group = ConstructionRobot
+    robot_entity.minable = robot_entity.minable
+    robot_entity.minable.result = robot_item.name
+
+    robot_entity.max_payload_size = c + hauler_entity.max_payload_size * c * modifier_max_payload_size
+    robot_entity.speed = s + hauler_entity.speed * s * modifier_speed
+
+    local energy = tonumber(string.match(robot_entity.max_energy, "%d+"))
+    local suffix = string.match(robot_entity.max_energy, "%a+")
+    robot_entity.max_energy = e + energy * e * modifier_max_energy .. suffix
+    return robot_item, robot_entity
+end
 
 function add_hauler_robots()
     for c=0, robot_cargo_research_limit do
         for s=0, robot_speed_research_limit do
             for e=0, robot_energy_research_limit do
-                local robot_item = table.deepcopy(hauler_item)
-                local robot_entity = table.deepcopy(hauler_entity)
-
-                local suffix = get_robot_suffix({cargo = c, speed = s, energy = e})
-                local name = combine{HaulerRobotLeveled, suffix}
-
-                robot_entity.localised_name = {"entity-name." .. HaulerRobotLeveled, tostring(c), tostring(s), tostring(e)}
-                robot_item.localised_name = {"item-name." .. HaulerRobotLeveled, tostring(c), tostring(s), tostring(e)}
-
-                robot_item.name = name
-                robot_entity.name = name
-                robot_item.hidden = true
-
-                robot_item.place_result = robot_entity.name
-                robot_entity.fast_replaceable_group = ConstructionRobot
-                robot_entity.minable = robot_entity.minable
-                robot_entity.minable.result = robot_item.name
-
-                robot_entity.max_payload_size = c + architect_entity.max_payload_size * c * modifier_max_payload_size
-                robot_entity.speed = s + architect_entity.speed * s * modifier_speed
-
-                local energy = tonumber(string.match(robot_entity.max_energy, "%d+"))
-                local suffix = string.match(robot_entity.max_energy, "%a+")
-                robot_entity.max_energy = e + energy * e * modifier_max_energy .. suffix
+                robot_item, robot_entity = create_hauler_robot(c, s, e)
 
                 if settings.startup[ShowItems].value == true then
                     robot_item.subgroup = ItemSubGroupRobot
